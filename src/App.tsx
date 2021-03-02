a399ae6902e1717a9f519df85a9be879371474bc
Pedro Murilo <pedromurilocalvi@gmail.com>
2021-03-02 15:25:51 -0300

    animation: all components on scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
-import { Main } from './styles';
-import GlobalStyles from './styles/GlobalStyles';
+import React from "react";
+import FirstAndSecond from "./components/FirstAndSecond";
+import { Main, Section, Sticky } from "./styles";
+import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
+  React.useEffect(() => {
+    window.scrollTo({ top: 0 })
+  }, [])
+
   return (
     <>
       <GlobalStyles />
-      <Main style={{ height: '1610vh' }}>
-        
+      <Main style={{ height: "1610vh" }}>
+        <Section style={{ height: '72.7%' }}>
+          <FirstAndSecond />
+        </Section>
+
+        <Section style={{ height: '9.7%' }}>
+          <Sticky className="third" />
+        </Section>
+
+        <Section style={{ height: '10.1%' }}>
+          <Sticky className="fourth" />
+        </Section>
       </Main>
     </>
   );
