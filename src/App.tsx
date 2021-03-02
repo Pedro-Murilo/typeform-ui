8630bc618aef9c1667d002a06b7ef7a193b7a5f2
Pedro Murilo <pedromurilocalvi@gmail.com>
2021-03-02 14:17:21 -0300

    styles: create global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Main } from './styles';
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <>
+      <GlobalStyles />
+      <Main style={{ height: '1610vh' }}>
+        
+      </Main>
+    </>
   );
 }
 
